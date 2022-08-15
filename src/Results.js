3fee65c215ac750e64f9c16e20860d5a17e24b02
SamC <villa151@umn.edu>
2022-08-15 12:30:06 -0500

    color and css
    changed color and initial look of app

--- src/Results.js
+++ src/Results.js
@@ -2,6 +2,8 @@ import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
 
+import "./Results.css";
+
 export default function Results(props) {
   if (props.results) {
     return (
