a8fc4b3b0bddab27b82bffbf932344e6add5edff
SamC <villa151@umn.edu>
2022-08-16 10:46:13 -0500

    alt
    added alt to img

--- src/Photos.js
+++ src/Photos.js
@@ -5,7 +5,7 @@ export default function Photos(props) {
     return (
       <section className="Photos">
         {props.photos.map(function (photo, index) {
-          return <img src={photo.src.tiny} key={index} />;
+          return <img src={photo.src.tiny} key={index} alt="Search result" />;
         })}
       </section>
     );
